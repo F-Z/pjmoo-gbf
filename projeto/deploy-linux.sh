9f779ae74ee327e404f491864e3865720504b506
davidferreira.FZ <davidferreira.FZ@11bb5d5c-9528-0410-9985-bbf9f5dd3cbf>
2007-06-21 02:49:58 +0000

    Migrando repositório do CódigoLivre para GoogleCode. Migração sendo feita da versão 0.6.1 da GBF, versões antigas podem ser encontradas no diretório tags

--- projeto/deploy-linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clear
-echo "Copiando Headers (.h)"
-cp -f  src/*.h /usr/include/GBF/
-echo "Copiando Biblioteca libGBF.a"
-cp -f  bin/linux/release/libGBF.a /usr/lib/libGBF.a
-#cp -f bin/linux/debug/libGBF.a /usr/lib/libGBF.a
-
