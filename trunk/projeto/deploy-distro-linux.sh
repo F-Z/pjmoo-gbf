cebb377615eb3667d5d2bd204f11020998eb4054
davidferreira.FZ <davidferreira.FZ@11bb5d5c-9528-0410-9985-bbf9f5dd3cbf>
2008-03-03 03:38:08 +0000

    Atualizando scripts de publicação

--- trunk/projeto/deploy-distro-linux.sh
+++ trunk/projeto/deploy-distro-linux.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+export GBF=/home/desenvolvimento/pjmoo-gbf/
+
+export GBF_CURRENT=$GBF/tags/current/
+export GBF_DEVELOPMENT=$GBF/trunk/projeto/
+
+export GBF_SRC=$GBF_DEVELOPMENT/src/
+export GBF_DISTRO_HEADERS=$GBF_CURRENT/include/
+
 clear
 echo "Preparando arquivos para o repositorio de distribuição..."
 
 echo "Copiando: Biblioteca libGBF.a"
-cp -f  bin/linux/release/libGBF.a /home/desenvolvimento/pjmoo/distro/lib/linux/release/libGBF.a
+cp -f $GBF_DEVELOPMENT/bin/linux/release/libGBF.a $GBF_CURRENT/linux/libGBF.a
 
 echo "Copiando: Headers (.h)"
-cp -f  src/*.h /home/desenvolvimento/pjmoo/distro/include/GBF
+cp -f  $GBF_SRC/*.h  $GBF_DISTRO_HEADERS
