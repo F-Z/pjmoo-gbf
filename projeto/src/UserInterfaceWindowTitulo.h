530c6c251cae00ea75dc3663655727540b9eea8d
davidferreira.FZ <davidferreira.FZ@11bb5d5c-9528-0410-9985-bbf9f5dd3cbf>
2008-05-10 03:18:03 +0000

    UserInterface: Atualizando componentes;

--- projeto/src/UserInterfaceWindowTitulo.h
+++ projeto/src/UserInterfaceWindowTitulo.h
@@ -1,5 +1,5 @@
 ////    GBF - Gamework's Brazilian Framework
-////    Copyright (C) 2004-2007 David de Almeida Ferreira
+////    Copyright (C) 2004-2008 David de Almeida Ferreira
 ////
 ////    This library is free software; you can redistribute it and/or
 ////    modify it under the terms of the GNU Library General Public
@@ -8,17 +8,17 @@
 ////
 ////    David de Almeida Ferreira (F-Z)
 ////        davidferreira@uol.com.br or davidferreira.fz@gmail.com
-////        http://pjmoo.codigolivre.org.br
 ////        http://pjmoo.sourceforge.net
+////        http://davidferreira-fz.blogspot.com
 ////////////////////////////////////////////////////////////////////////
 
 #ifndef _USERINTERFACEWINDOWTITULO_H
 #define _USERINTERFACEWINDOWTITULO_H
 
-#include "UserInterfaceWindow.h"
 #include "UserInterfaceObjetoTexto.h"
+#include "UserInterfaceWindowDialog.h"
 
-class UserInterfaceWindowTitulo : public UserInterfaceWindow
+class UserInterfaceWindowTitulo : public UserInterfaceWindowDialog
 {
   public:
     UserInterfaceWindowTitulo();
