2d2866a2e518300b9f76dd7ebcb73c0587948895
davidferreira.FZ <davidferreira.FZ@11bb5d5c-9528-0410-9985-bbf9f5dd3cbf>
2010-03-22 00:31:42 +0000

    Sincronizando arquivos de configuração do Projeto

--- projeto/src/version.h
+++ projeto/src/version.h
@@ -14,17 +14,17 @@ namespace AutoVersion{
 	static const long REVISION = 3722;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 726;
+	static const long BUILDS_COUNT = 728;
 	#define RC_FILEVERSION 0,9,660,3722
 	#define RC_FILEVERSION_STRING "0, 9, 660, 3722\0"
 	static const char FULLVERSION_STRING[] = "0.9.660.3722";
 	
 	//SVN Version
-	static const char SVN_REVISION[] = "193";
-	static const char SVN_DATE[] = "2010-03-18T02:58:59.918992Z";
+	static const char SVN_REVISION[] = "241";
+	static const char SVN_DATE[] = "2009-08-15T01:26:48.777071Z";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 660;
+	static const long BUILD_HISTORY = 0;
 	
 
 }
